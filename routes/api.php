eb51c3e2e77cae42eb80d0f5f84a0ac4ed06714a
Vanessa Steffen <vanessasteffen186@gmail.com>
2022-06-20 16:31:09 -0300

    20/06/2022

--- routes/api.php
+++ routes/api.php
@@ -10,13 +10,13 @@ use App\Http\Controllers\Api\VinculoApiController;
 //Route:: get('/produtos', [ProdutoApiControllerbkp::class, 'index']);
 //Route:: get('/clientes', [ClienteApiController::class, 'index']);
 
+Route:: get('clientes/{id}/vinculo', [ClienteApiController::class, 'vinculo']);
 Route:: get('clientes/{id}/produtos', [ClienteApiController::class, 'produtos']);
 Route:: apiResource('clientes', ClienteApiController::class);
 
 Route:: get('vinculo/{id}/clientes', [VinculoApiController::class, 'clientes']);
-Route:: get('vinculo/{id}/produtos', [VinculoApiController::class, 'produtos']);
-Route::apiResource('produtos', ProdutoApiController::class);
-Route::apiResource('produtos', ProdutoApiController::class);
+Route::apiResource('vinculo', VinculoApiController::class);
+
 
 
 Route:: get('produtos/{id}/clientes', [ProdutoApiController::class, 'clientes']);
